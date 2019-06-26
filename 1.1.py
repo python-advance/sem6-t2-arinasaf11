8ee20a1dc134392682f554ba2a010add3a6940b5
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2019-06-26 08:07:45 +0300

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -2,10 +2,14 @@ lst = [0, 1, 1]
 n = int(input('Введите количество чисел: '))
 fib1, fib2 = 1, 1
 
-if n < 3:
-  print(lst)
+if n == 0:
+  quit()
+if n == 1:
+  print(lst[0])
+if n == 2:
+  print(lst[0:2])
 else:
   for i in range(3, n):
    fib1, fib2 = fib2, fib1 + fib2
    lst.append(fib2)
-print(lst)
+  print(lst)
