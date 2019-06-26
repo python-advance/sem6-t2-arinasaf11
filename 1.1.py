f1202ec952c313ed92281303510945049e944a21
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2019-06-26 08:11:53 +0300

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -1,15 +1,18 @@
-lst = [0, 1, 1]
-n = int(input('Введите количество чисел: '))
-fib1, fib2 = 1, 1
-
-if n == 0:
-  quit()
-if n == 1:
-  print(lst[0])
-if n == 2:
-  print(lst[0:2])
-else:
-  for i in range(3, n):
-   fib1, fib2 = fib2, fib1 + fib2
-   lst.append(fib2)
+def fib(n):
+  lst = [0, 1, 1]
+  fib1, fib2 = 1, 1
+  if n == 0:
+    quit()
+  if n == 1:
+    print(lst[0])
+  if n == 2:
+    print(lst[0:2])
+  else:
+    for i in range(3, n):
+      fib1, fib2 = fib2, fib1 + fib2
+      lst.append(fib2)
   print(lst)
+  return lst
+
+n = int(input('Введите количество чисел: '))
+fib(n)
